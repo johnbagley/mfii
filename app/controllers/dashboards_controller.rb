597edf577bef7ad69ccbd84fd3a9e6c53e818a6f
John Bagley <jdbagley@gmail.com>
2014-08-14 12:56:30 -0400

    Add Teams

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,3 +1,6 @@
 class DashboardsController < ApplicationController
-
+  def show
+    @team = Team.new
+    @teams = current_user.teams
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140814161231) do
+ActiveRecord::Schema.define(version: 20140814165356) do
+
+  create_table "teams", force: true do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "teams", ["user_id"], name: "index_teams_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email"
